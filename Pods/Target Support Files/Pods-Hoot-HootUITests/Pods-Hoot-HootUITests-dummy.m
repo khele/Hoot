38c60b80e75a7dd243a03a65bcbd960e6cf09b36
Kristian <kristian.helenius@hotmail.com>
2019-09-01 20:39:52 +0300

    second commit

---
+++ Pods/Target Support Files/Pods-Hoot-HootUITests/Pods-Hoot-HootUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hoot_HootUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Hoot_HootUITests
+@end
---
+++ Pods/Target Support Files/Pods-Hoot/Pods-Hoot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hoot : NSObject
+@end
+@implementation PodsDummy_Pods_Hoot
+@end
